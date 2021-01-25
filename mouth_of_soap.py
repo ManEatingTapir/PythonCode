318d263979399bc7f465ba83b1c9132673833dd1
David Spivey <davidspivey3.1415@gmail.com>
2021-01-25 16:47:27 -0600

    Update mouth_of_soap to return a list of strings w/info instead of boolean.

--- mouth_of_soap.py
+++ mouth_of_soap.py
@@ -2,24 +2,21 @@
 import re
 from pathlib import Path
 
-# TODO: find way to integrate this with git precommit hooks so it runs whenever try to do a commit
-
 def soap(file):
+    """Uses regex to parse a file for any expletives. Returns a list of strings with the line each occurence is on."""
     # Check if it's a directory
     if Path(file).is_dir():
         raise IsADirectoryError
-    # assume it doesn't have any swears
-    bool_flag = True
+    # list to hold any output
+    output = []
     with open(file) as f:
         # Check each line of file, index is used to indicate which line it's on (add one since iterator starts at 0)
         for index, line in enumerate(f):
-            # if there is a match
+            # TODO: make it parse for other swears. Keep some sort of list of patterns?
             if re.search('fu+ck', line):
-                print(f'Cleanup crew on line {index + 1}')
-                bool_flag = False
-    return bool_flag
+                output.append(f'Cleanup crew on line {index + 1} in {file}')
+    return output
 
-            
 # example code
 if __name__ == '__main__':
     print(soap('ascii.py'))
